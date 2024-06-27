1a2b7498cb6874b5e87e8934d93913df873c5974
westvillage-jp <77133766+westvillage-jp@users.noreply.github.com>
2024-06-27 17:52:20 +0900

    20240627kadai

---
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="view-port"  content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <h1>投稿一覧</h1>
+    </body>
+</html>
+
