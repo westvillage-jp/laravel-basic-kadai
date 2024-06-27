023ef030b074e003f9bf954413fd3c8780e4ba9a
westvillage-jp <77133766+westvillage-jp@users.noreply.github.com>
2024-06-27 17:54:44 +0900

    20240627kadai

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [HelloController::class, 'index']);
+
+Route::get('/posts', [PostController::class, 'index']);
